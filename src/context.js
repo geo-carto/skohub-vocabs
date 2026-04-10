0a795d5b70d4e6c3faa3db1f6168686e93fa92cb
geo-carto <cartogeoigme@gmail.com>
2026-04-10 19:01:27 +0200

    Add DCAT namespace to context.js

--- src/context.js
+++ src/context.js
@@ -11,6 +11,7 @@ const jsonld = {
     vann: "http://purl.org/vocab/vann/",
     ldp: "http://www.w3.org/ns/ldp#",
     owl: "http://www.w3.org/2002/07/owl#",
+    dcat: "http://www.w3.org/ns/dcat#",
     title: {
       "@id": "dct:title",
       "@container": "@language",
